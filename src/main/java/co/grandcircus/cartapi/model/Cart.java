f954e9c18fd756a543b9f8b74ed962448f653a4e
miraj <miraj00@gmail.com>
2023-02-21 13:14:10 -0500

    cart model added

--- src/main/java/co/grandcircus/cartapi/model/Cart.java
+++ src/main/java/co/grandcircus/cartapi/model/Cart.java
@@ -1,7 +1,60 @@
 package co.grandcircus.cartapi.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cart_items")
 public class Cart {
 
-
-	
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private long id;
+		private String product;
+		private Double price;
+		private int quantity;
+		
+		
+		public long getId() {
+			return id;
+		}
+		public void setId(long id) {
+			this.id = id;
+		}
+		public String getProduct() {
+			return product;
+		}
+		public void setProduct(String product) {
+			this.product = product;
+		}
+		public Double getPrice() {
+			return price;
+		}
+		public void setPrice(Double price) {
+			this.price = price;
+		}
+		public int getQuantity() {
+			return quantity;
+		}
+		public void setQuantity(int quantity) {
+			this.quantity = quantity;
+		}
+		
+		
+		public Cart() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+		
+		public Cart(long id, String product, Double price, int quantity) {
+			super();
+			this.id = id;
+			this.product = product;
+			this.price = price;
+			this.quantity = quantity;
+		}
+			
 }
